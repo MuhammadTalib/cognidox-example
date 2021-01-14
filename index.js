6dc0b43f6db979eefea11643df168b390557b86e
Muhammad Talib Waseem <talibwaseem135@gmail.com>
2021-01-14 19:43:17 +0500

    cognidox

---
+++ index.js
@@ -0,0 +1,23 @@
+const { execSync } = require('child_process');
+
+const commandInvoker =  (command) => {
+    let result =  null;
+    try{
+        result = execSync(command);
+    }
+    catch(Error){
+        result = Error.stdout;
+    }
+    return result;
+};
+
+function cognidox_transform(filename) {
+    let Path_arr = filename.split(".")
+    let doc_extention = Path_arr[Path_arr.length - 1]
+    command = "cognidox.exe " + Path_arr[0] + "." + doc_extention+ " " + Path_arr[0] + ".pdf";
+    let result = commandInvoker(command);
+    
+    console.log(result.toString());
+}
+
+cognidox_transform("doscumessnt.docx");
