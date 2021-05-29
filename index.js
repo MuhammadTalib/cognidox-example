baf9843d643fcd7368a604ed2b2ebc7641fb20e7
Muhammad Talib Waseem <talibwaseem135@gmail.com>
2021-05-29 17:21:54 +0500

    cognidox

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const { execSync } = require('child_process');
 
-const commandInvoker =  (command) => {
-    let result =  null;
-    try{
-        result = execSync(command);
-    }
-    catch(Error){
-        result = Error.stdout;
-    }
-    return result;
-};
+// const commandInvoker =  (command) => {
+//     let result =  null;
+//     try{
+//         result = execSync(command);
+//     }
+//     catch(Error){
+//         result = Error.stdout;
+//     }
+//     return result;
+// };
 
-function cognidox_transform(filename) {
-    let Path_arr = filename.split(".")
-    let doc_extention = Path_arr[Path_arr.length - 1]
-    command = "cognidox.exe " + Path_arr[0] + "." + doc_extention+ " " + Path_arr[0] + ".pdf";
-    let result = commandInvoker(command);
+// function cognidox_transform(filename) {
+//     let Path_arr = filename.split(".")
+//     let doc_extention = Path_arr[Path_arr.length - 1]
+//     command = "cognidox.exe " + Path_arr[0] + "." + doc_extention+ " " + Path_arr[0] + ".pdf";
+//     let result = commandInvoker(command);
     
-    console.log(result.toString());
-}
+//     console.log(result.toString());
+// }
 
-cognidox_transform("doscumessnt.docx");
+// cognidox_transform("doscumessnt.docx");
+
+const fs = require('fs')
+
+const content = 'Some content!'
+
+try {
+  const data = fs.writeFileSync('tes.docx', content)
+  //file written successfully
+} catch (err) {
+  console.error(err)
+}
